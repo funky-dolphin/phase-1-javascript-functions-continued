e400c6fefa1510f8262115607db4fa2dccd0589a
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-02-01 13:28:18 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+    }
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+
+function wrapAdjective(flex= "*"){
+    return function encouragingPromptFunction(celcius = "Special"){
+    return `You are ${flex}${celcius}${flex}!`;
+    };
+}
